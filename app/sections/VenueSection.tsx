852a17336c77f0453e010aa2cdaca45a31fdc177
Lucas van der Kleij <kleijvander@gmail.com>
2025-07-05 13:16:58 +0200

    .

--- app/sections/VenueSection.tsx
+++ app/sections/VenueSection.tsx
@@ -18,7 +18,6 @@ export const VenueSection: React.FC = () => {
     <section ref={ref} className="atlas-container mt-[24rem]">
       <section className="atlas-grid">
         <Header />
-        <VerticalCaption />
 
         <ImageBlock
           src="/image/sample.jpg"
@@ -29,33 +28,40 @@ export const VenueSection: React.FC = () => {
           aspectRatio={1.3}
         />
 
+        <div className="col-span-full col-start-4 lg:col-start-6 row-start-3 flex justify-end mt-[0.4rem]">
+          <h1 className="caption flex flex-col">
+            <span>The heart of our</span>
+            <span>wedding weekend</span>
+          </h1>
+        </div>
+
         <ImageBlock
           src="/image/sample.jpg"
           avif="/image/lacoste-1.avif"
           motionStyle={isLarge ? { y: y2 } : {}}
-          className="relative block col-span-6 col-start-3 sm:col-start-2 sm:col-span-5 lg:col-start-2 lg:col-span-3 row-start-3 lg:row-start-2 mt-[4rem]"
+          className="relative block col-span-5 col-start-3 sm:col-start-2 sm:col-span-5 lg:col-start-2 lg:col-span-3 row-start-4 lg:row-start-2 mt-[4rem]"
           overlayOpacity={0.2}
-          aspectRatio={0.8}
+          aspectRatio={0.7}
         />
 
         <TextBlock
           title="The venue"
           text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo similique nam libero aliquam cupiditate. Quidem eius atque tempore placeat quam ducimus nostrum quod, labore magni ratione mollitia reiciendis recusandae veniam?"
           link="https://www.google.com/maps/place//data=!4m2!3m1!1s0x12ab590d5edb6237:0x9247997e9a5c89b9?sa=X&ved=1t:8290&ictx=111"
-          className="atlas-grid grid-cols-12 sm:grid-cols-5 lg:grid-cols-5 col-span-full col-start-1 sm:col-start-7 sm:col-span-5 lg:col-start-8 lg:col-span-5 row-start-4 lg:row-start-2 mt-[4rem]"
+          className="atlas-grid grid-cols-12 sm:grid-cols-5 lg:grid-cols-5 col-span-full col-start-1 sm:col-start-7 sm:col-span-5 lg:col-start-8 lg:col-span-5 row-start-5 lg:row-start-2 mt-[4rem]"
         />
 
         <TextBlock
           title="The lodging"
           text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo similique nam libero aliquam cupiditate. Quidem eius atque tempore placeat quam ducimus nostrum quod, labore magni ratione mollitia reiciendis recusandae veniam?"
-          className="atlas-grid grid-cols-12 sm:grid-cols-5 lg:grid-cols-5 col-span-full col-start-1 sm:col-start-3 sm:col-span-5 lg:col-start-2 lg:col-span-5 row-start-6 lg:row-start-3 mt-[4rem]"
+          className="atlas-grid grid-cols-12 sm:grid-cols-5 lg:grid-cols-5 col-span-full col-start-1 sm:col-start-3 sm:col-span-5 lg:col-start-2 lg:col-span-5 row-start-7 lg:row-start-3 mt-[4rem]"
         />
 
         <ImageBlock
           src="/image/sample.jpg"
           avif="/image/lacoste-3.avif"
           motionStyle={isLarge ? { y: y3 } : {}}
-          className="relative block col-span-9 sm:col-span-8 lg:col-span-5 col-start-4 sm:col-start-5 lg:col-start-8 row-start-5 lg:row-start-4 mt-[6rem]"
+          className="relative block col-span-9 sm:col-span-8 lg:col-span-5 col-start-4 sm:col-start-5 lg:col-start-8 row-start-6 lg:row-start-4 mt-[6rem]"
           overlayOpacity={0.2}
           aspectRatio={1.2}
         />
@@ -65,26 +71,10 @@ export const VenueSection: React.FC = () => {
 };
 
 const Header = () => (
-  <h2 className="heading-2 flex flex-col self-end lg:self-center col-span-10 col-start-1 lg:col-span-4 row-start-1 mb-[1rem]">
+  <h2 className="heading-2 flex flex-col self-end lg:self-center col-span-12 col-start-1 lg:col-span-4 row-start-1 mb-[1rem]">
     <span>Chateau</span>
     <span>de Lacoste</span>
   </h2>
-);
-
-const VerticalCaption = () => (
-  <h1
-    className="caption flex items-end rotate-180 mt-[40%] col-span-1 col-start-12 lg:col-start-6 row-start-1 origin-center mb-[6rem]"
-    style={{
-      writingMode: 'vertical-rl',
-      textOrientation: 'mixed',
-      height: 'fit-content',
-    }}
-  >
-    the Zugspitze peak rises dramatically
-    <br />
-    over Alpine pastures
-    <br />
-  </h1>
 );
 
 interface ImageBlockProps {
@@ -128,19 +118,45 @@ interface TextBlockProps {
   className: string;
 }
 
-const TextBlock: React.FC<TextBlockProps> = ({ title, text, link, className }) => (
-  <div className={className}>
-    <h2 className="heading-3 col-start-1 row-start-1 -col-end-2">{title}</h2>
-    <p className="text col-start-2 row-start-2 -col-end-2">{text}</p>
-    {link && (
-      <div className="row-start-3 col-start-2 -col-end-2 mt-[1rem]">
-        <a href={link} className="link-detail inline-block ">
-          To the venue
-        </a>
-      </div>
-    )}
-  </div>
-);
+const TextBlock: React.FC<TextBlockProps> = ({ title, text, link, className }) => {
+  const ref = useRef(null);
+  const words = title.split(' ');
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start 90%', 'start 10%'], // triggers later
+  });
+
+  return (
+    <div className={className}>
+      <h2 ref={ref} className="heading-3 col-start-1 row-start-1 -col-end-2 flex flex-wrap gap-x-2">
+        {words.map((wordText, index) => {
+          const step = 0.15; // space between words
+          const start = index * step;
+          const end = start + 0.05; // short fade duration
+
+          const opacity = useTransform(scrollYProgress, [start, end], [0.2, 0.8]);
+
+          return (
+            <motion.span key={index} style={{ opacity }} className="inline-block">
+              {wordText}
+            </motion.span>
+          );
+        })}
+      </h2>
+
+      <p className="text col-start-2 row-start-2 -col-end-2">{text}</p>
+
+      {link && (
+        <div className="row-start-3 col-start-2 -col-end-2 mt-[1rem]">
+          <a href={link} className="link-detail inline-block">
+            To the venue
+          </a>
+        </div>
+      )}
+    </div>
+  );
+};
 
 function useIsLargeScreen() {
   const [isLarge, setIsLarge] = useState(false);
